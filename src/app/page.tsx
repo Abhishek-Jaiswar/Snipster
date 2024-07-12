2834b5c01207eccbd3945864c820e594d8a2d87c
Abhishek Jaiswar <abhisheknduw@gmail.com>
2024-07-12 21:10:11 +0530

    testing collab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import Link from "next/link"
 
 export default function ButtonDemo() {
   return (
-    <div className="h-screen flex flex-col items-center gap-2 justify-center ">
-      <h1 className="text-3xl font-bold text-primary capitalize">Hi Folks, This is Mock.io the interview simulator</h1>
-      <p className="text-sm font-semibold text-zinc-400">Welcome to mock.io, Explore the dashboard by clicking the visit button</p>
+    <div className="h-screen flex flex-col items-center md:gap-2 justify-center ">
+      <h1 className=" text-2xl md:text-3xl text-center p-2 md:p-0 font-bold text-primary capitalize">Hi Folks, This is Mock.io the interview simulator</h1>
+      <p className="text-sm font-semibold text-center p-2 md:p-0 text-zinc-400">Welcome to mock.io, Explore the dashboard by clicking the visit button</p>
       <Link href={'/dashboard'}>
         <Button>Vist Dashboard</Button>
       </Link>
