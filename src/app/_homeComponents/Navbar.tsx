f04309f7615623b07eda86dda6639c04a206feb9
Abhishek Jaiswar <abhisheknduw@gmail.com>
2024-08-28 14:32:26 +0530

    fixes navbar

--- src/app/_homeComponents/Navbar.tsx
+++ src/app/_homeComponents/Navbar.tsx
@@ -1,116 +1,15 @@
-"use client"
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import React from 'react'
 
-import Link from "next/link"
-import {
-    CodeXml,
-    Home,
-    LayoutDashboard,
-    LineChart,
-    Menu,
-    Package,
-    Package2,
-    ShoppingCart,
-    Users,
-} from "lucide-react"
-
-import { Badge } from "@/components/ui/badge"
-
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-
-import { usePathname } from "next/navigation"
-import { Themes } from "../dashboard/_components/Themes"
-
-
-const Header = () => {
-    const pathname = usePathname()
+const Navbar = () => {
     return (
-        <header className="flex h-14 sticky z-50 top-0 left-0 items-center gap-4 border-b px-4 lg:h-[60px] lg:px-6">
-            <Sheet>
-                <SheetTrigger asChild>
-                    <span className="md:hidden">
-                        <Menu />
-                    </span>
-                </SheetTrigger>
-                <SheetContent side="left" className="flex flex-col">
-                    <nav className="grid gap-2 text-lg font-medium">
-                        <div className="flex items-center justify-between text-lg font-semibold mb-5">
-                            <div className="flex items-center justify-center gap-2">
-                                <CodeXml className="h-6 w-6" />
-                                <h1 className=" font-bold">Snipster</h1>
-                            </div>
-                        </div>
-                        <Link href="/dashboard/overview" className={` ${pathname === '/dashboard/overview' ? "bg-muted text-white" : ""} mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2  hover:text-foreground text-muted-foreground `}>
-                            <LayoutDashboard className="h-4 w-4" />
-                            Overview
-                        </Link>
+        <div>
+            <Link href="/sign-in">
+                <Button className=''>Login</Button>
+            </Link>
+        </div>
+    )
+}
 
-                        <Link href="/dashboard/my-interviews" className={` ${pathname === '/dashboard/my-interviews' ? "bg-muted text-white" : ""} mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2  hover:text-foreground text-muted-foreground`}>
-                            <ShoppingCart className="h-5 w-5" />
-                            My Interviews
-                            <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">6</Badge>
-                        </Link>
-
-                        <Link href="/dashboard/start-interview" className={` ${pathname === '/dashboard/start-interview' ? "bg-muted text-foreground text-white" : ""} mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground`}>
-                            <Package className="h-5 w-5" />
-                            Start Interview
-                        </Link>
-
-                        <Link href="/dashboard/manage-questions" className={` ${pathname === '/dashboard/manage-questions' ? "bg-muted  text-white" : ""} mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground`}>
-                            <Users className="h-5 w-5" />
-                            Manage questions
-                        </Link>
-
-                        <Link href="/dashboard/analytics" className={` ${pathname === '/dashboard/analytics' ? "bg-muted text-white" : ""} mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground`}>
-                            <LineChart className="h-5 w-5" />
-                            Analytics
-                        </Link>
-                    </nav>
-                </SheetContent>
-            </Sheet>
-            <div className="w-[85%] mx-auto flex items-center justify-between" >
-                <div className="flex items-center justify-center gap-2">
-                    <CodeXml className="h-7 w-7 font-bold" />
-                    <h1 className=" font-bold">Snipster</h1>
-                </div>
-                <div className="hidden md:block">
-                    <div className="flex flex-1 items-center justify-center gap-5">
-                        <Link href="/dashboard/overview" className={` ${pathname === '/dashboard/overview' ? "bg-muted text-white" : ""} mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2  hover:text-foreground text-muted-foreground `}>
-                            Overview
-                        </Link>
-
-                        <Link href="/dashboard/my-interviews" className={` ${pathname === '/dashboard/my-interviews' ? "bg-muted text-white" : ""} mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2  hover:text-foreground text-muted-foreground`}>
-                            My Interviews
-                        </Link>
-
-                        <Link href="/dashboard/start-interview" className={` ${pathname === '/dashboard/start-interview' ? "bg-muted text-foreground text-white" : ""} mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground`}>
-                            Start Interview
-                        </Link>
-
-                        <Link href="/dashboard/manage-questions" className={` ${pathname === '/dashboard/manage-questions' ? "bg-muted  text-white" : ""} mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground`}>
-                            Manage questions
-                        </Link>
-
-                        <Link href="/dashboard/analytics" className={` ${pathname === '/dashboard/analytics' ? "bg-muted text-white" : ""} mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground`}>
-                            Analytics
-                        </Link>
-                    </div>
-                </div>
-                <div className="flex items-center justify-center gap-5">
-                    <div>
-                        <Themes />
-                    </div>
-                    <div className="flex items-center justify-center gap-5">
-                        <Link href={'/dashboard'}>
-                            <button className="dark:bg-primary bg-gray-800 text-white dark:text-black px-2 py-1 rounded-md hover:bg-gray-50">Login</button>
-                        </Link>
-                        <Link href={'/sign-up'}>
-                            <button className="dark:bg-primary bg-gray-800 text-white dark:text-black px-2 py-1 rounded-md hover:bg-gray-50">Sign Up</button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </header>
-    );
-};
-
-export default Header;
+export default Navbar
